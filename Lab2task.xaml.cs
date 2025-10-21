fe699db0d1744afd4817efbb788a8d42b608daf1
andrezzzw01-creator <andrezzzw01@gmail.com>
2025-10-21 21:12:39 +0800

    Add files via upload

---
+++ Lab2task.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MauiApp2
+{
+    // Must inherit from ContentPage
+    public partial class Lab2task : ContentPage
+    {
+        public Lab2task()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿namespace MauiApp2
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        // Change from 'void' to 'async void'
+        private async void OnCounterClicked(object sender, EventArgs e)
+        {
+            // 1. Create an instance of the target page
+            var secondPage = new Lab2task();
+
+            // 2. Navigate to the new page using PushAsync
+            await Navigation.PushAsync(secondPage);
+
+            // You can optionally keep the counter logic if you want:
+            // count++;
+            // if (count == 1)
+            //     CounterBtn.Text = $"Clicked {count} time";
+            // else
+            //     CounterBtn.Text = $"Clicked {count} times";
+            // SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+    }
+}
+
